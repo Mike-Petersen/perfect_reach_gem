20921e62a472c337d67f9d1ddc1ead9e60b28a55
Mike Petersen <mike@odania-it.de>
2015-06-18 23:39:26 +0200

    Updated api_error class

--- lib/perfect_reach/api_error.rb
+++ lib/perfect_reach/api_error.rb
@@ -5,13 +5,10 @@ module PerfectReach
 	class ApiError < StandardError
 		def initialize(code, res, request, request_path, params)
 			# code is ugly, output is pretty
+			error_text = res['errors'].present? ? [(res['errors'] || [])].flatten.map {|param, text| [param, text].map(&:to_s).reject(&:blank?).join(': ')}.join("\n") : res.inspect
+
 			super("error #{code} while sending #{request.inspect} to #{request_path} with #{params.inspect}\n\n" +
-						(res['errors'].present? ?
-							[(res['errors'] || [])].flatten.map do |param, text|
-								[param, text].map(&:to_s).reject(&:blank?).join(': ')
-							end.join("\n") :
-							res.inspect
-						) +
+						error_text +
 						"\n\nPlease see http://api.perfect-reach.com for more informations on error numbers.\n\n"
 			)
 		end
